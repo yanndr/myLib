8fcb452e67622c786edf9f918ccb7e0f90a637dc
Yann Druffin <yann.druffin@gmail.com>
2023-04-10 20:45:16 -0700

    Created the base of API

--- api/cmd/myLibApi/main.go
+++ api/cmd/myLibApi/main.go
@@ -1,5 +1,72 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "port default 8080")
+	flag.Parse()
+
+	if err := run(port); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(port int) error {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(10 * time.Second))
+	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("ok"))
+	})
+
+	server := &http.Server{Addr: fmt.Sprintf(":%v", port), Handler: r}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	go func() {
+		<-sig
+		log.Println("Stopping the server...")
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, cancel := context.WithTimeout(serverCtx, 30*time.Second)
+		defer cancel()
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+	fmt.Printf("Server started, listening on port: %v\n", port)
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+	return nil
 }
